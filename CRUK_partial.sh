b111d997056abc2a248895b050ce8f475180b94c
Sara Rey <sara.rey.manchester@gmail.com>
2017-07-05 11:45:30 +0100

    Working pairign with text file

--- CRUK_partial.sh
+++ CRUK_partial.sh
@@ -8,28 +8,31 @@ Version=0.0
 
 
 # How to use
-# bash CRUK_draft.sh <path_to_sample_sheet> <path_to_results_location> <config_file_name> <Sample Pairs text file>
+# bash CRUK_draft.sh <path_to_sample_sheet> <path_to_results_location> <config_file_name> <name_of_negative_control_sample> <sample_pairs_text_file>
 # /Users/sararey/Documents/cruk_test_data/rawFQs/ # for reference- path to sample sheet and fastqs
 # Requires bash version 4 or above
 
 # Usage checking
-if [ "$#" -lt 2 ]
+if [ "$#" -lt 3 ]
 	then
-		echo "Commandline args incorrect. Usage: $0 <path_to_sample_sheet> <path_to_results_location>." 
+		echo "Commandline args incorrect. Usage: $0 <path_to_sample_sheet> <path_to_results_location> <name_of_negative_control_sample>." 
 		exit 0
 fi
 
-if [ "$#" -lt 3 ]
+if [ "$#" -lt 4 ]
 	then
 		SAMPLEPAIRS="SamplePairs.txt"
 	else
-		SAMPLEPAIRS="$3"
+		SAMPLEPAIRS="$4"
+		# Include code to skip generation of a SamplePairs.txt file
 fi
+
 
 # Variables
 APPNAME="SMP2 v2"
 INPUTFOLDER="$1"
 RESULTSFOLDER="$2"
+NEGATIVE="$3"
 
 
 # Declare an associative array to store the patient name and sample id
@@ -45,6 +48,7 @@ samplesArr+=1 # Hack around an issue- fix this later
 function parseSampleSheet {
 
 	echo "Parsing sample sheet"
+	>samples.txt
 	
 	# Obtain project name from sample sheet
 	#projectName=$(grep "Experiment Name" "$INPUTFOLDER"SampleSheet.csv | cut -d, -f2 | tr -d " ")
@@ -69,44 +73,20 @@ function parseSampleSheet {
 
 		# Append information to list array- to retain order for sample pairing
 		samplesArr=("${samplesArr[@]}" "$samplename")
-	done
 
+		# Append information to file- to retain order for sample pairing
+		printf '%s\n' "$samplename">>samples.txt
+	done
 }
+
 
 function pairSamples {
 
 	echo "Pairing samples"
 
 	# Create/clear file which holds the sample name and the patient identifiers
-	>"$SAMPLEPAIRS"
-
-	#count=0
-	
-	#echo "${samplesArr[@]}"
-
-	#awk -v var="${!samplePatient[*]}" 'NR % 2 {print var}'	
-	awk -F '\t' 'NR % 2 {printf "%s\t", $1;} !(NR % 2) {printf "%s\n", $1;}' samples.txt
-	
-	awk -F '\t' 'NR % 2 {printf "%s\t", $1;} !(NR % 2) {printf "%s\n", $1;}' samples.txt >$SAMPLEPAIRS
-	
-	#for sample in ${!samplePatient[@]}
-	#do
-
-		#echo $sample	
-		#if (( $count % 2 == 0 ))
-	 	#then
-	 	 	#tumour="$sample"
-	 	#else
-	 	 	#normal="$sample"
-	 	 	# Add paired samples to a file- tumour sample first, normal sample second
-	 	 	#printf "$tumour">>"$SAMPLEPAIRS"
-	 	 	#printf "%s\t">>"$SAMPLEPAIRS"
-	 	 	#printf "$normal">>"$SAMPLEPAIRS"
-	 	 	#printf "%s\n">>"$SAMPLEPAIRS"
-		#fi
-		#((count++))
-	#done
-
+	> "$SAMPLEPAIRS"
+	grep -f not_bs_samples.txt -v samples.txt | awk -F '\t' 'NR % 2 {printf "%s\t", $1;} !(NR % 2) {printf "%s\n", $1;}' > "$SAMPLEPAIRS"
 }
 
 
@@ -118,10 +98,8 @@ function locateFastqs {
 	do
 		f1=$INPUTFOLDER${fastq}*_R1_*.fastq.gz
 		f2=$INPUTFOLDER${fastq}*_R2_*.fastq.gz
-
 	done
 }
-
 
 
 # Call the functions
@@ -138,17 +116,18 @@ pairSamples
 # Get fastqs
 locateFastqs $INPUTFOLDER
 
+echo ${samplesArr[@]:1}
 
 # Launch app for each pair of samples in turn as tumour normal pairs then download analysis files
 echo "Launching app"
 while read pair
 do
-	printf $pair	
-	tum=$(printf "$pair" | cut -d" " -f1)
-	nor=$(printf "$pair" | cut -d" " -f2)
+	tum=$(printf "$pair" | cut -d$'\t' -f1)
+	nor=$(printf "$pair" | cut -d$'\t' -f2)
 
-	echo $tum
-	echo $nor
+	echo "negative " $NEGATIVE  	
+	echo "tumour "$tum
+	echo "normal "$nor
 
 done <"$SAMPLEPAIRS"
 
